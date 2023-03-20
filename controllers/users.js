c96a2f787db1bdd3bcf671740225f6befed3f8af
Ritesh <ratohikumar@gmail.com>
2023-03-20 23:50:19 +0530

    some minor changes

--- controllers/users.js
+++ controllers/users.js
@@ -1,31 +1,51 @@
 const User = require('../models/user');
 
 module.exports.renderRegister = (req, res) => {
-    res.render('users/register')
+    if (!req.user) {
+
+        return res.render('users/register');
+    } else {
+        req.flash('error', 'You are already logged IN ')
+        res.redirect('/items');
+    }
 }
 
 module.exports.register = async (req, res) => {
-    try {
-        const { email, username, password } = req.body;
-        const user = new User({ email, username });
-        const registeredUser = await User.register(user, password);
-        req.login(registeredUser, err => {
-            if (err) return next(err);
+   
+    if (!req.user) {
+        try {
+            const { email, username, password } = req.body;
+            const user = new User({ email, username });
+            const registeredUser = await User.register(user, password);
+            req.login(registeredUser, err => {
+                if (err) return next(err);
 
-            // console.log(registeredUser);
-            req.flash('success', 'Welcome to Menu!');
-            res.redirect('/items');
-        })
+                // console.log(registeredUser);
+                req.flash('success', 'Welcome to Menu!');
+                res.redirect('/items');
+            })
+        }
+        catch (e) {
+            req.flash('error', e.message);
+            res.redirect('/register');
+        }
+    } else {
+        req.flash('error', 'You are already logged IN ')
+        res.redirect('/items');
     }
-    catch (e) {
-        req.flash('error', e.message);
-        res.redirect('/register');
-    }
+    
 
 }
 
 module.exports.renderLogin = (req, res) => {
-    res.render('users/login');
+    if (!req.user) {
+        
+        return res.render('users/login');
+    } else {
+        req.flash('error', 'You are already logged IN ')
+        return res.redirect('/items');
+    }
+    
 }
 
 module.exports.login = async (req, res) => {
